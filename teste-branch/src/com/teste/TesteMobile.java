ccfffef19c0b39202d3f861de372a84672b89dec
VyMajoris <VyMajoris>
2014-02-17 03:48:06 -0300

    Cleaning

--- teste-branch/src/com/teste/TesteMobile.java
+++
@@ -1,8 +0,0 @@
-package com.teste;
-
-public class TesteMobile {
-	public static void main(String[] args) {
-		System.out.println("teste");
-	}
-
-}
--- teste-branch/src/com/teste/testeMobile2.java
+++
@@ -1,7 +0,0 @@
-package com.teste;
-
-public class testeMobile2 {
-public static void main(String[] args) {
-	System.out.println("2");
-}
-}
