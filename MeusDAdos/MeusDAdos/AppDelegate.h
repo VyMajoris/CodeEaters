fce9b05c02a77a4ffbad9724b35294475ebca3eb
VyMajoris <VyMajoris>
2014-02-17 03:21:04 -0300

    TESTE COM O XCODE5

---
+++ MeusDAdos/MeusDAdos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MeusDAdos
+//
+//  Created by VyMajoris on 2/17/14.
+//  Copyright (c) 2014 VyMajoris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
