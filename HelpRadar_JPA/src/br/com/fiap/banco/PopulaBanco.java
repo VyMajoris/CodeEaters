ab8251cbd9f44fce8257c77c5c554c54672748ec
VyMajoris <vycanismajoriscsa@gmail.com>
2014-09-23 23:44:51 -0300

    Update PopulaBanco.java

--- HelpRadar_JPA/src/br/com/fiap/banco/PopulaBanco.java
+++ HelpRadar_JPA/src/br/com/fiap/banco/PopulaBanco.java
@@ -35,146 +35,163 @@ public class PopulaBanco {
 
 	public static void main(String[] args) {
 		EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager();
-		
-		
-		 //INICIO da persistencia de 3 especialidades 
-		 
+
+
+		//INICIO da persistencia de 3 especialidades 
+
 		Especialidade especialidade1 = new Especialidade("Encanador");
 		Especialidade especialidade2 = new Especialidade("Mecanico");
 		Especialidade especialidade3 = new Especialidade("Programador");
-		
+
 		EspecialidadeDAO especialidadeDAO = new EspecialidadeDAOImpl(em);
 		especialidadeDAO.insert(especialidade1);
 		especialidadeDAO.insert(especialidade2);
 		especialidadeDAO.insert(especialidade3);
-		
+
+
 		//FIM da persistencia das 3 especialidades
-	
-		
-		
+
+
+
 		//INICIO da persistencia de 3 Avaliacoes
 		Calendar c = Calendar.getInstance();
-		
-		
+
+
 		Avaliacao avaliacao1 = new Avaliacao("Muito Bom", c, "Titulo1", 5);
 		Avaliacao avaliacao2 = new Avaliacao("Bom", c, "Titulo2", 3 );
 		Avaliacao avaliacao3 = new Avaliacao("Sem avaliacao", c, "Titulo3", null);
-		
+
 		AvaliacaoDAO avaliacaoDAO = new AvaliacaoDAOImpl(em);
 		avaliacaoDAO.insert(avaliacao1);
 		avaliacaoDAO.insert(avaliacao2);
 		avaliacaoDAO.insert(avaliacao3);
-		
+
 		//FIM da persistencia de 3 Avaliacoes
-		
+
 		//INICIO da persistencia de 3 Contato
 		List<String> listEmail = new ArrayList<>();
 		listEmail.add("emailUser1@gmail.com");
-		
+
 		List<String> listTel = new ArrayList<>();
 		listTel.add("56611941");
-		
+
 		List<String> listEmail2 = new ArrayList<>();
 		listEmail.add("emailUser2@gmail.com");
-		
+
 		List<String> listTel2 = new ArrayList<>();
 		listTel.add("56611942");
-		
+
 		List<String> listTel3 = new ArrayList<>();
 		listTel.add("12345673");
-		
+
 		List<String> listEmail3 = new ArrayList<>();
 		listEmail.add("emailUser3@gmail.com");
-		
-		
+
+
 		Contato contato1 = new Contato(listEmail, listTel, null, true);
 		Contato contato2 = new Contato(listEmail2, listTel2, null, true);
 		Contato contato3 = new Contato(listEmail3, listTel3, null, true);
-		
+
 		ContatoDAO contatoDAO = new ContatoDAOImpl(em);
 		contatoDAO.insert(contato1);
 		contatoDAO.insert(contato2);
 		contatoDAO.insert(contato3);
-		
+
 		//FIM da persistencia de 3 Contato
-		
-		
+
+
 		//INSERE 3 USUÁRIOS DEFAULT
 		Usuario usuario1 = new Usuario(1, "Pedro Henrique", false, null, contato1, TipoUsuario.DEFAULT);
 		List<Usuario> listAmg2 = new ArrayList<>();
 		listAmg2.add(usuario1);
-		
+
 		Usuario usuario2 = new Usuario(2, "Felipe Jose", false, listAmg2, contato2, TipoUsuario.DEFAULT);
 		List<Usuario> listAmg3 = new ArrayList<>();
 		listAmg3.add(usuario2);
 
 		Usuario usuario3 = new Usuario(3, "Eduardo",false, listAmg3, contato3, TipoUsuario.DEFAULT);
-		
-		
-				
+
+
+
 		UsuarioDAO usuarioDAO = new UsuarioDAOImpl(em);
 		usuarioDAO.insert(usuario1);
 		usuarioDAO.insert(usuario2);
 		usuarioDAO.insert(usuario3);
 		//FIM  3 USUÁRIOS DEFAULT
-		
-	
-		
-		
-		
+
+
+
+
+
 		//INICIO da persistencia de 3 ASSISTENTE	
 		List<Avaliacao> listAvaiacao1 = new ArrayList<>();
 		listAvaiacao1.add(avaliacao1);
-		
+
 		List<Avaliacao> listAvaiacao2 = new ArrayList<>();
 		listAvaiacao2.add(avaliacao2);
-		
+
 		List<Avaliacao> listAvaiacao3 = new ArrayList<>();
 		listAvaiacao3.add(avaliacao3);
-		
-		
-		
-		
+
+
+
+
 		//FIM da persistencia de 3 ASSISTENTE
-		
+
 		//INICIO da persistencia de 3 Identificacao	
 		List<String> listPecas1 = new ArrayList<>();
 		listPecas1.add("ListaDePecas1?");
-				
+
 		List<String> listPecas2 = new ArrayList<>();
 		listPecas2.add("ListaDePecas2?");
-		
+
 		List<String> listPecas3 = new ArrayList<>();
 		listPecas3.add("ListaDePecas3?");
-		
+
 		Identificacao identificacao1 = new Identificacao( listPecas1);
 		Identificacao identificacao2 = new Identificacao( listPecas2);
 		Identificacao identificacao3 = new Identificacao( listPecas3);
-				
+
 		IdentificacaoDAO identificacaoDAO = new IdentificacaoDAOImpl(em);
 		identificacaoDAO.insert(identificacao1);
 		identificacaoDAO.insert(identificacao2);
 		identificacaoDAO.insert(identificacao3);
 		//FIM da persistencia de 3 Identificacao
-		
-		
-		
-		
-		
+
+
+
+
+
 		//UPGRADE DE 2 USUÁRIOS PARA ASSISTENTE
-				usuario2.setTipoUsuario(TipoUsuario.ASSISTENTE);
-				usuario2.setDiaLogado(true);
-				usuario2.setIdentificacao(identificacao2);
-				usuario2.setListaAvaliacoes(listAvaiacao2);
-				
-				usuario3.setTipoUsuario(TipoUsuario.ASSISTENTE);
-				usuario3.setDiaLogado(true);
-				usuario3.setIdentificacao(identificacao3);
-				usuario3.setListaAvaliacoes(listAvaiacao3);
-				
-				usuarioDAO.update(usuario2);
-				usuarioDAO.update(usuario3);
-				
+
+		List<Especialidade> listEspecialidade1 = new ArrayList<>();
+		listEspecialidade1.add(especialidade3);
+		listEspecialidade1.add(especialidade2);
+		listEspecialidade1.add(especialidade1);
+
+		List<Especialidade> listEspecialidade2 = new ArrayList<>();
+		listEspecialidade2.add(especialidade1);
+		listEspecialidade2.add(especialidade2);
+		listEspecialidade2.add(especialidade3);
+
+
+		usuario2.setTipoUsuario(TipoUsuario.ASSISTENTE);
+		usuario2.setDiaLogado(true);
+		usuario2.setIdentificacao(identificacao2);
+		usuario2.setListaAvaliacoes(listAvaiacao2);
+		usuario2.setListaEspecialidade(listEspecialidade1);
+		usuarioDAO.update(usuario2);
+
+		usuario3.setTipoUsuario(TipoUsuario.ASSISTENTE);
+		usuario3.setDiaLogado(true);
+		usuario3.setIdentificacao(identificacao3);
+		usuario3.setListaAvaliacoes(listAvaiacao3);
+		usuario3.setListaEspecialidade(listEspecialidade2);
+		usuarioDAO.update(usuario3);
+
+
+
+
 	}
 
 }
